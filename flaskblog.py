cd6d7127f67832d712d090a1464dfebf0649760e
vidit-agarwal <viditvivo@gmail.com>
2018-08-24 13:43:33 +0530

    Updating The User Login/Reg System

--- flaskblog.py
+++ flaskblog.py
@@ -1,44 +1,52 @@
-from flask import Flask , render_template , url_for
-from forms import RegistrationForm , LoginForm
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-app.config['SECRET_KEY'] = 'e027fac140c77ea54a438617de678178'
 posts = [
     {
-        'author' : 'Vidit Agarwal' ,
-        'title' : 'Blog Post' ,
-        'content' : 'This is the first blog of the site' ,
-        'date_posted' : 'April 21, 2018'
-    } ,
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
     {
-        'author' : 'Swetabh Pathak' ,
-        'title' : 'Elucidata Information' ,
-        'content' : 'I am the Co-Founder and the Directior of the Elucidata Inc' ,
-        'date_posted' : 'Jan 11, 2015'
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html' , posts=posts)
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html' , title='About') 
+    return render_template('about.html', title='About')
 
-@app.route("/register")
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html' , title='Register', form=form)
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html' , title='Login', form=form)
-    
-
-
-#if __name__ == '__main__' :
- #   app.run(debug=True)
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
