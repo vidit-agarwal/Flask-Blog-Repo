8a4cf6aaea8daa38ef86bda7461a8377db5831d0
vidit-agarwal <viditvivo@gmail.com>
2018-08-24 13:19:56 +0530

    Adding User Registeration System

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask , render_template , url_for
+from forms import RegistrationForm , LoginForm
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'e027fac140c77ea54a438617de678178'
 posts = [
     {
         'author' : 'Vidit Agarwal' ,
@@ -24,6 +27,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html' , title='About') 
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html' , title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html' , title='Login', form=form)
+    
 
 
 #if __name__ == '__main__' :
